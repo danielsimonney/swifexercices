68c43a461b874cac735612e2999c7a7892f8f232
danielsimonney <daniel.simonney60@gmail.com>
2019-10-17 14:50:19 +0200

    Initial Commit

---
+++ swiftExercice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftExercice
+//
+//  Created by daniel on 17/10/2019.
+//  Copyright © 2019 daniel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
